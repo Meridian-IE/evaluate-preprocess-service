9e9800625da5c7d72b534076fecf766d80d7bd94
Julian Gruber <julian@juliangruber.com>
2023-08-27 21:42:36 +0200

    add basic evaluate

--- index.js
+++ index.js
@@ -1,26 +1,7 @@
-import { ethers } from 'ethers'
-import fs from 'node:fs/promises'
-import { fileURLToPath } from 'node:url'
+
 import { preprocess } from './lib/preprocess.js'
 import { evaluate } from './lib/evaluate.js'
-
-const {
-  RPC_URL = 'https://api.calibration.node.glif.io/rpc/v0',
-  IE_CONTRACT_ADDRESS = '0xedb63b83ca55233432357a7aa2b150407f8ea256',
-} = process.env
-
-// Set up contract
-const provider = new ethers.providers.JsonRpcProvider(RPC_URL)
-const ieContract = new ethers.Contract(
-  IE_CONTRACT_ADDRESS,
-  JSON.parse(
-    await fs.readFile(
-      fileURLToPath(new URL('./abi.json', import.meta.url)),
-      'utf8'
-    )
-  ),
-  provider
-)
+import { ieContract } from './lib/contract.js'
 
 const db = {
   aggregates: []
@@ -40,9 +21,5 @@ ieContract.on('RoundStart', _roundIndex => {
   console.log('Event: RoundStart', { roundIndex })
 
   // Evaluate previous round
-  evaluate({
-    db,
-    ieContract,
-    roundIndex: roundIndex - 1
-  }).catch(console.error)
+  evaluate({ db, roundIndex: roundIndex - 1 }).catch(console.error)
 })
