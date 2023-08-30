d58d937880e57438e73b0f8aaf2ac93f1f61f6f4
Julian Gruber <julian@juliangruber.com>
2023-08-30 12:25:07 +0200

    fix redundant evaluations

--- index.js
+++ index.js
@@ -4,22 +4,32 @@ import { evaluate } from './lib/evaluate.js'
 import { ieContract } from './lib/contract.js'
 
 const db = {
-  aggregates: []
+  aggregates: [],
+  cidsSeen: [],
+  roundsSeen: []
 }
 
 // Listen for events
-ieContract.on('MeasurementAdded', (cid, _roundIndex) => {
+ieContract.on('MeasurementsAdded', (cid, _roundIndex) => {
   const roundIndex = Number(_roundIndex)
-  console.log('Event: MeasurementAdded', { roundIndex })
+  if (db.cidsSeen.includes(cid)) {
+    return
+  }
+  db.cidsSeen.push(cid)
 
+  console.log('Event: MeasurementsAdded', { roundIndex })
   // Preprocess
   preprocess({ db, cid, roundIndex }).catch(console.error)
 })
 
 ieContract.on('RoundStart', _roundIndex => {
   const roundIndex = Number(_roundIndex)
-  console.log('Event: RoundStart', { roundIndex })
+  if (db.roundsSeen.includes(roundIndex)) {
+    return
+  }
+  db.roundsSeen.push(roundIndex)
 
+  console.log('Event: RoundStart', { roundIndex })
   // Evaluate previous round
   evaluate({ db, roundIndex: roundIndex - 1 }).catch(console.error)
 })
